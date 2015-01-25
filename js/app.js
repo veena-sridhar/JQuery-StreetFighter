07c204842c03797db5a875413c3039ea1fe3ad57
veena-sridhar <v.sridhar@gmail.com>
2015-01-24 17:45:36 -0800

    Finished Street Fighter Project.

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	$(document.body).keydown(function (e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	$(document.body).keyup(function (e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').show();
+			$('.ryu-cool').hide();
+		}
+	})
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show()
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	$('.ryu').mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+});
+
+function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+}
